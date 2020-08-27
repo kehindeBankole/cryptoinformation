2d766eebb92296ccf27ea8d22bf90a84b7c0f085
kehindeBankole <bankolek1@gmail.com>
2020-08-27 08:28:06 +0100

    put data in table and designed table

--- src/components/People.js
+++ src/components/People.js
@@ -1,22 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { makeapicall } from '../redux/people api/actions'
 function People() {
-    const data = useSelector(state => state.data)
+    const data = useSelector(state => state)
     const dispa = useDispatch()
+    useEffect(() => {
+        dispa(makeapicall())
 
-      useEffect(() => {
-          dispa(makeapicall())
-         
-      }, [])
-      console.log(data)
+    }, [])
+    console.log(data)
     return (
-        <div>
-            {
-                data.map((d)=>{
-                     return d.id
-                })
-            }
+        <div className="table-responsive  table-responsive-sm table-responsive-md">
+            <table class="table table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">s/n</th>
+                        <th scope="col">symbol</th>
+                        <th scope="col">coin</th>
+                        <th scope="col">current price</th>
+                        <th scope="col">total volume</th>
+                        <th scope="col">total supply</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.data.map((d , index)=>{
+                      return(
+                        <tr>
+                        <th scope="row">{index}</th>
+                      <td><img src={d.image} alt="coin symbol" width="40" height="40"/></td>
+                      <td>{d.name}</td>
+                      <td>{d.current_price}</td>
+                      <td>{d.total_volume}</td>
+                      <td>{d.total_supply === null ? 'not available' : d.total_supply}</td>
+                    </tr> 
+                      )
+                    })}
+                </tbody>
+            </table>
+
         </div>
     )
 }
